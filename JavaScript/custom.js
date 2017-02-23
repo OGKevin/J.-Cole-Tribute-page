6dbf1196a3431a90e53372e868efe1fa2b7c4b80
OGKevin <Kevinhellemun@icloud.com>
2017-02-23 16:54:39 +0100

    Update using JSON

--- JavaScript/custom.js
+++ JavaScript/custom.js
@@ -1,181 +1,84 @@
-$(function () {
-	function hideMain() {
-		$("#mainDiv").fadeOut('slow', function () {});
-	}
+$(function() {
+    var info = (function() {
+            var json = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': "./JavaScript/info.json",
+                'dataType': "json",
+                'success': function(data) {
+                    json = data;
+                }
+            });
+            return json;
+        })(),
+        i = 0;
 
-	function hideAll() {
-		$("#comeup, #thewarmup, #FDL, #sideline, #sinner, #truly, #FHD, #eyez").fadeOut('fast', function () {
-			console.log("Hiden");
-		});
-	}
-	hideAll();
-	// NOTE: begining sidebar actions
-	$("#homeSide").click(function (event) {
-		hideAll();
-		$("#wrap").css('background-image', 'url("images/j_cole_final_by_sbm832-d96e2ll.jpg")');
-		$("#mainDiv").fadeIn('fast', function () {});
-		console.log("homeBtn");
-	});
-	$("#comeUpSide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://4.bp.blogspot.com/_zhTSVuwwoNE/TF-hmiqYZbI/AAAAAAAAAEY/Z0pj06Njt0Q/s1600/The+Come+Up+(Hosted+By+DJ+On+Point)+3.jpg")');
-		$("#comeup").fadeIn('fast', function () {});
-		console.log("ComeUpBtn");
-	});
-	$("#theWarmUpSide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://static.vibe.com/files/article_images/J-Cole-The-Warm-Up.jpg")');
-		$("#thewarmup").fadeIn('fast', function () {});
-		console.log("theWarmUpBtn");
-	});
-	$("#trulySide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/9/98/Cole-Truly-Yours.jpeg")');
-		$("#truly").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	$("#FDLside").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://bespokemag.co.uk/wp-content/uploads/2015/11/fnl.jpg")');
-		$("#FDL").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	$("#sideLineSide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://images.rapgenius.com/3cgkifu1zzc0hjclvad6k1tov.1000x1000x1.jpg")');
-		$("#sideline").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	$("#sinnerSide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://beginningandend.com/wp-content/uploads/2013/06/J-Cole-Born-Sinner-Devil-Head-Illuminati-Satanic-Sybolism.jpg")');
-		$("#sinner").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	$("#FHDside").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("https://images.genius.com/7154c6dafd6f6f883c099f033a4aa5e3.1000x1000x1.jpg")');
-		$("#FHD").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	$("#eyezSide").click(function (event) {
-		hideAll();
-		hideMain();
-		$("#wrap").css('background-image', 'url("http://dreamville.com/wp-content/uploads/2016/12/4UEO_Cvr-e1480572866398.jpg")');
-		$("#eyez").fadeIn('fast', function () {});
-		console.log("trulySideImg");
-	});
-	// NOTE: end sidebar
-	// NOTE: scrolling currently trash
-	/*$("#mainDiv").on( 'DOMMouseScroll mousewheel', function ( event ) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('2000', function() {});
-	    $("#comeup").show('slow', function() {
+    function hideMain() {
+        $("#mainDiv").fadeOut();
+    };
 
-	  console.log('Down');
-	})} else {
-	    //scroll up
-	    console.log('Up');
-	  }
-	//prevent page fom scrolling
-	return false;
-	});
-
-	$("#comeup").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#thewarmup").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#mainDiv").show('slow', function() {});
-	      console.log("up");
-	    }
-	    return false;
-
-	  });
-
-	$("#thewarmup").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#FDL").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#comeup").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });
-	$("#FDL").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#sideline").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#thewarmup").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });
-	$("#sideline").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#truly").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#FDL").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });
-	$("#truly").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#FHD").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#sideline").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });
-	$("#FHD").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    $(this).hide('slow', function() {});
-	    $("#eyez").show('slow', function() {
-	    });
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#truly").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });
-	$("#eyez").on('DOMMouseScroll mousewheel', function(event) {
-	  if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-	    console.log("Down");
-	  } else {
-	      $(this).hide('slow', function() {});
-	      $("#FHD").show('slow', function() {});
-	      console.log("up");
-	    }
-
-	  });*/
+    function changePage(infoNR) {
+        $("#albumDiscription").html(info[infoNR].discription);
+        $("#alnumTitle").html(info[infoNR].title);
+        $("#albumArt").attr("src", info[infoNR].AlbumArtUrl);
+        $("#albumArt").attr("alt", info[infoNR].title + " IMG");
+        $("#iframe").attr("src", info[infoNR].embendedPlayer);
+        $(".wikiLink").attr('href', info[infoNR].wikiLink);
+    }
+    // NOTE: begining sidebar actions
+    $("#homeSide").click(function(event) {
+        $("#wrap").css('background-image', 'url("images/j_cole_final_by_sbm832-d96e2ll.jpg")');
+        $("#mainDiv").fadeIn("slow", function() {});
+        $("#info").css('visibility', 'hidden');
+    });
+    $("#comeUpSide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://4.bp.blogspot.com/_zhTSVuwwoNE/TF-hmiqYZbI/AAAAAAAAAEY/Z0pj06Njt0Q/s1600/The+Come+Up+(Hosted+By+DJ+On+Point)+3.jpg")');
+        hideMain();
+        changePage(0);
+    });
+    $("#theWarmUpSide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://static.vibe.com/files/article_images/J-Cole-The-Warm-Up.jpg")');
+        changePage(1);
+        hideMain();
+    });
+    $("#trulySide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/9/98/Cole-Truly-Yours.jpeg")');
+        changePage(5);
+        hideMain();
+    });
+    $("#FDLside").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://bespokemag.co.uk/wp-content/uploads/2015/11/fnl.jpg")');
+        changePage(2);
+        hideMain();
+    });
+    $("#sideLineSide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://images.rapgenius.com/3cgkifu1zzc0hjclvad6k1tov.1000x1000x1.jpg")');
+        changePage(3);
+        hideMain();
+    });
+    $("#sinnerSide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://beginningandend.com/wp-content/uploads/2013/06/J-Cole-Born-Sinner-Devil-Head-Illuminati-Satanic-Sybolism.jpg")');
+        changePage(4);
+        hideMain();
+    });
+    $("#FHDside").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("https://images.genius.com/7154c6dafd6f6f883c099f033a4aa5e3.1000x1000x1.jpg")');
+        changePage(6);
+        hideMain();
+    });
+    $("#eyezSide").click(function(event) {
+        $("#info").css('visibility', 'visible');
+        $("#wrap").css('background-image', 'url("http://dreamville.com/wp-content/uploads/2016/12/4UEO_Cvr-e1480572866398.jpg")');
+        changePage(7);
+        hideMain();
+    });
+    // NOTE: end sidebar
 });
--- README.md
+++ README.md
@@ -7,15 +7,27 @@ Sellin' dope, sellin' dope, sellin' dope
 Well motherfucker, I am
 
 ##Links
+
 Version 1: (http://ogkevintribute.surge.sh)
 
 Latest version: (https://ogkevin.github.io/J.-Cole-Tribute-page/)
 
 ##Known bugs
 
+<<<<<<< HEAD
 * White line at thop of page
 * Transitions aren't smooth
 * Embanded datpiff player doesn't load due HTTP over HTTPS
 * Grey boxes are to big
 * Used wrong technique to hide the elements at page first load
 * Looks rubbish on smaller screens 
+=======
+* ~~White line at top of page.~~
+* ~~Transitions aren't smooth.~~ Way better due to JSON but still not animated.
+* Embanded datpiff player doesn't load due HTTP over HTTPS.
+* Grey boxes are to big.
+* ~~Used wrong technique to hide the elements at page first load.~~
+* Looks rubbish on smaller screens.
+* Side bar changes size when the "#info" div gets bigger or smaller.
+* "#info" div is to far to the right due invisible territory of the sidebar.
+>>>>>>> 30257ac... styling update
